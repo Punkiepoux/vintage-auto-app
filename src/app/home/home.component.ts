3729bff6703d2de22f85a53670d0c3b1cb2ccfe5
Antoine Dausque <antoine.dausque@hotmail.fr>
2024-08-09 11:11:51 +0200

    ajout de swiper + changement logique affichage contact

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import Swiper from 'swiper';
+import { Autoplay, Navigation } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit {
+  ngAfterViewInit(): void {
+    const swiper = new Swiper('.swiper', {
+      modules: [Autoplay, Navigation],
 
+      slidesPerView: 1,
+      autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+      },
+      effect: 'slide',
+      loop: true,
+      simulateTouch: true,
+
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+
+    });
+  }
 }
