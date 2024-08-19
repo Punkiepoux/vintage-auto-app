6a2e09e946b20d9849613dea8d6e5564ebb61bb1
Antoine Dausque <antoine.dausque@hotmail.fr>
2024-08-19 21:34:00 +0200

    all api.service.ts

---
+++ src/app/services/api-services.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiServicesService {
+  private apiUrl = 'http://localhost:3000/api';
+
+  constructor(private http: HttpClient) { }
+
+  getUserInfo(): Observable<any> {
+    return this.http.get(`${this.apiUrl}/services`);
+  }
+}
